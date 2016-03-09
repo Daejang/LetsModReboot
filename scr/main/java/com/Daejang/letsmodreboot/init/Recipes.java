f00bf8c15f44a186addade1670611eacea4687ef
Daejang <Kalby@Kalbys-MacBook-Pro.local>
2016-03-09 09:52:16 -0800

    Finished making the recipes
    This is the end of Pahimar’s Let Mod Reboot series.  I will need
    additional help from other people.

--- scr/main/java/com/Daejang/letsmodreboot/init/Recipes.java
+++ scr/main/java/com/Daejang/letsmodreboot/init/Recipes.java
@@ -1,12 +1,14 @@
 package com.Daejang.letsmodreboot.init;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 public class Recipes {
 
     public static void init() {
 
-//        GameRegistry.addRecipe(new ItemStack(ModItems.), "");
+        GameRegistry.addRecipe(new ItemStack(ModItems.wonderIngotItem), " i ", " d ", " g ", 'i', new ItemStack(Items.iron_ingot), 'd', new ItemStack(Items.diamond), 'g', new ItemStack(Items.gold_ingot));
+        GameRegistry.addRecipe(new ItemStack(ModItems.pickaxeItem), "www", " s ", " s ", 'w', new ItemStack(ModItems.wonderIngotItem), 's', new ItemStack(Items.stick));
     }
 }
